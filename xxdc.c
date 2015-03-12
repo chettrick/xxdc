c97e18cead564ad58cbe45bb198d76f68cd17d11
Chris Hettrick <git@structfoo.com>
2015-03-12 01:56:02 -0700

    More flensing of xxdc. No need to specify an `outfile'. This is UNIX! Use shell redirection. Add the removal of outfile to the manpage, and also specify what happens when using standard input. Add the proper year to the manpage. Clean up err() messages. Just use 1 as the return value for error. Inline usage(). #define COLS.

--- xxdc.c
+++ xxdc.c
@@ -1,5 +1,6 @@
 /*
- * (c) 1990-1998 by Juergen Weigert (jnweiger@informatik.uni-erlangen.de)
+ * Copyright (c) 2015 Chris Hettrick <chris@structfoo.com>
+ * (c) 1990-1998 by Juergen Weigert <jnweiger@informatik.uni-erlangen.de>
  *
  * Distribute freely and credit me,
  * make money and share with me,
@@ -8,113 +9,71 @@
 
 #include <ctype.h>
 #include <err.h>
-#include <fcntl.h>
 #include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-static void usage(void)__attribute__((__noreturn__));
+#define COLS 12
 
 int
 main(int argc, char *argv[])
 {
 	FILE *		fp;
-	FILE *		fpo = stdout;
-	char *		pp;
 	char *		bname;
 	int		c, e;
 	uint64_t	count;
-	unsigned int	cols = 12;
 
-	while (argc >= 2) {
-		pp = argv[1] + (!strncmp(argv[1], "--", 2) && argv[1][2]);
-		if (!strncmp(pp, "-i", 2)) {		/* continue on -i */
-			argc--;
-			argv++;
-			break;
-		} else if (!strcmp(pp, "--")) {		/* end of options */
-			argc--;
-			argv++;
-			break;
-		} else if (pp[0] == '-' && pp[1])	/* unknown option */
-			usage();
-			/* NOTREACHED */
-		else
-			break;				/* not an option */
+	if (argc > 2) {
+		(void)fprintf(stderr, "usage: xxdc [infile]\n");
+		exit(1);
 	}
-
-	if (argc > 3)
-		usage();
-		/* NOTREACHED */
-
 	if (argc == 1 || (argv[1][0] == '-' && !argv[1][1]))
 		fp = stdin;
 	else
 		if ((fp = fopen(argv[1], "r")) == NULL)
-			err(2, "%s", argv[1]);
-
-	if (argc < 3 || (argv[2][0] == '-' && !argv[2][1]))
-		fpo = stdout;
-	else {
-		int fd;
-
-		if (((fd = open(argv[2], O_WRONLY | O_CREAT, 0666)) < 0) ||
-		    (fpo = fdopen(fd, "w")) == NULL)
-			err(3, "%s", argv[2]);
-		if (fpo)
-			rewind(fpo);
-	}
+			err(1, "%s", argv[1]);
 
 	if ((bname = basename(argv[1])) == NULL)
 		err(1, "%s", argv[1]);
 
 	if (fp != stdin) {
-		if (fprintf(fpo, "unsigned char %s",
+		if (fprintf(stdout, "unsigned char %s",
 		    isdigit((int)bname[0]) ? "__" : "") < 0)
-			err(3, NULL);
+			err(1, "stdout");
 		for (e = 0; (c = (int)bname[e]) != 0; e++)
-			if (putc(isalnum(c) ? c : '_', fpo) == EOF)
-				err(3, NULL);
-		if (fprintf(fpo, "[] = {\n") == EOF)
-			err(3, NULL);
+			if (putc(isalnum(c) ? c : '_', stdout) == EOF)
+				err(1, "stdout");
+		if (fprintf(stdout, "[] = {\n") == EOF)
+			err(1, "stdout");
 	}
 
 	for (count = 1; (c = getc(fp)) != EOF; count++)
-		if (fprintf(fpo, "0x%02x,%s", c,
-		    ((count % 12) > 0) ? " " : "\n") < 0)
-			err(3, NULL);
+		if (fprintf(stdout, "0x%02x,%s", c,
+		    ((count % COLS) > 0) ? " " : "\n") < 0)
+			err(1, "stdout");
 
 	if (c == EOF && ferror(fp))
-		err(2, NULL);
-	if (count && fprintf(fpo, "%s",
-	    (count % cols) == 1 ? "" : "\n") == EOF)
-		err(3, NULL);
-	if (fprintf(fpo, "%s", (fp == stdin) ? "" : "};\n") == EOF)
-		err(3, NULL);
+		err(1, "stdin");
+	if (fprintf(stdout, "%s",
+	    (count % COLS) == 1 ? "" : "\n") == EOF)
+		err(1, "stdout");
+	if (fprintf(stdout, "%s", (fp == stdin) ? "" : "};\n") == EOF)
+		err(1, "stdout");
 
 	if (fp != stdin) {
-		if (fprintf(fpo, "unsigned int %s",
+		if (fprintf(stdout, "unsigned int %s",
 		    isdigit((int)bname[0]) ? "__" : "") < 0)
-			err(3, NULL);
+			err(1, "stdout");
 		for (e = 0; (c = (int)bname[e]) != 0; e++)
-			if (putc(isalnum(c) ? c : '_', fpo) == EOF)
-				err(3, NULL);
-		if (fprintf(fpo, "_len = %llu;\n", count - 1) < 0)
-			err(3, NULL);
+			if (putc(isalnum(c) ? c : '_', stdout) == EOF)
+				err(1, "stdout");
+		if (fprintf(stdout, "_len = %llu;\n", count - 1) < 0)
+			err(1, "stdout");
 	}
 
 	if (fclose(fp))
-		err(2, NULL);
-	if (fclose(fpo))
-		err(3, NULL);
+		err(1, "stdin");
+	if (fclose(stdout))
+		err(1, "stdout");
 	return (0);
-}
-
-static void
-usage(void)
-{
-	(void)fprintf(stderr, "usage: xxdc [infile [outfile]]\n");
-	exit(1);
-	/* NOTREACHED */
 }
